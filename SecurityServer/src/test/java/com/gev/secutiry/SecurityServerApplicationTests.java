7e051e8b74c1657eec6b8ad644a8768ce93d3caf
g3v <egnandal500@yahoo.com>
2021-11-07 19:56:09 +0100

    Microservices de gestion des étudiants : Emploi du temps, informations et resultats

---
+++ SecurityServer/src/test/java/com/gev/secutiry/SecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gev.secutiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
